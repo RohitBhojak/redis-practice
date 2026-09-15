afc7ed103cf7f0555381c36d4282aabcd46afe2e
Rohit <rohitbhojak99@gmail.com>
2026-09-15 12:27:45 +0000

    Integrate redis locally

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const { default: axios } = require("axios");
 const express = require("express");
+const { createClient } = require("redis");
+
+const PORT = 3000;
+const DEFAULT_EXPIRY_TIME = 3600;
 
 const app = express();
+const redisClient = createClient();
 
-app.get("/", async (req, res) => {
-  try {
-    const { data } = await axios.get("https://jsonplaceholder.typicode.com/photos");
-    return res.send(data);
-  } catch (err) {
-    console.error(err);
+const startRedis = async () => await redisClient.connect();
+
+startRedis();
+
+app.get("/photos", async (req, res) => {
+  const cached = await redisClient.get("photos");
+
+  if (cached != null) {
+    console.log("Cache Hit");
+    return res.json(JSON.parse(cached));
   }
+
+  console.log("Cache Miss");
+  const { data } = await axios.get("https://jsonplaceholder.typicode.com/photos");
+  await redisClient.setEx("photos", DEFAULT_EXPIRY_TIME, JSON.stringify(data));
+  res.json(data);
 });
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     console.error(err);
   }
-  console.log("Listening on port: 3000");
+  console.log("Listening on port:", PORT);
 });
